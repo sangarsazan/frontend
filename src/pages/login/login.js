12db6aa15bb0e9fc8a4fd217e20879d7a63acee3
Developer-web2023 <Master.Developer23@gmail.com>
2023-11-01 22:32:19 +0330

    Login page with out fonts

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import {TextField, Typography} from "@mui/material";
-import styles from "./login.module.css";
+import Styles from "./login.module.css";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import Button from "@mui/material/Button";
+import ButtonArrow from "@mui/material/Button";
+import Button from "../../components/button";
 const LoginPage = () => {
     return (
         <div>
-            <div className={styles.forward}>
-                <Button style={{color:"black"}} className={styles.btnForward}><ArrowForwardIcon fontSize="large"/></Button>
+            <div style={{padding: "20px 10px 0 0"}}>
+                <ButtonArrow style={{color:"black"}} className={Styles.btnForward}><ArrowForwardIcon fontSize="medium"/></ButtonArrow>
             </div>
 
              <div>
-                 <Typography className={styles.Welcome} variant="h4" fontWeight="bold">خوش برگشتی!</Typography>
+                 <Typography className={Styles.Welcome} variant="h4" fontWeight="bold">خوش برگشتی!</Typography>
              </div>
 
             <div>
                  <Typography variant="h5" style={{textAlign: "center" , padding: "20px"}}>برای ورود ، اطلاعات زیر را وارد کنید</Typography>
             </div>
-            <div style={{display: "flex", padding:"100px 0 15px 0" , justifyContent: "center"}}>
+
+            <div className={Styles.phone}>
                 <TextField id="outlined-basic" label="شماره تلفن" variant="outlined" style={{textAlign:"left"}}/>
             </div>
+
             <div style={{display: "flex", justifyContent: "center"}}>
-                <TextField id="outlined-basic" label="گذرواژه" variant="outlined"/>
+                <TextField id="outlined-basic" label="گذرواژه" variant="outlined" type="password"/>
             </div>
+
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <Button className={Styles.button} text={'ورود'}></Button>
+            </div>
+
         </div>
     );
 };
