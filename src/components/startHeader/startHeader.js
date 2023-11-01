eadea421dab114a1f4661bd66b77a6013e3127b3
Developer-web2023 <Master.Developer23@gmail.com>
2023-11-01 17:47:26 +0330

    2651154

--- src/components/startHeader/startHeader.js
+++ src/components/startHeader/startHeader.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import styles from './startHeader.module.css';
 const StartHeader = ({text}) => {
     return (
         <>
+            <div style={{display: "flex"}}>
                 <svg data-testid={CloseIcon}></svg>
                 <span>{text}</span>
+            </div>
         </>
     );
 };
