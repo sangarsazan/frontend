c653a751a897958d980a63dad2f6dfcc664d0ace
trumanshoow <dehghanalireza1214@gmail.com>
2023-11-01 16:26:39 +0330

    start

---
+++ src/pages/signUp/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Styles from './signup.module.css'
+
+const startPage = () => {
+    return (
+        <>
+            <img className={Styles.logo} src={'#'} alt={"logo"}/>
+
+        </>
+    );
+};
+
+export default startPage;
