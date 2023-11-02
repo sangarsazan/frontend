f0336fd1b7742f7ab5fe40717c9b265f2561be98
trumanshoow <dehghanalireza1214@gmail.com>
2023-11-02 09:36:06 +0330

    ...10

---
+++ src/components/stepBar/index.js
@@ -0,0 +1,14 @@
+import Styles from './stepBar.module.css'
+
+
+const StepBar = () => {
+    return (
+        <div className={Styles.step}>
+            <div className={Styles.nextBlue}>
+
+            </div>
+        </div>
+    );
+};
+
+export default StepBar;
