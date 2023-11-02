1df8a899ec7ce1288f2e8a2362af8b2963dd94a0
Developer-web2023 <Master.Developer23@gmail.com>
2023-11-03 02:22:11 +0330

    5

--- src/pages/members/index.js
+++ src/pages/members/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Appbar from "../../components/Appbar/Appbar";
-import Styles from "../home/home.module.css";
+import Styles from "./members.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMagnifyingGlass, faPowerOff} from "@fortawesome/free-solid-svg-icons";
 import AppHeader from "../../components/AppHeader/AppHeader";
@@ -9,17 +9,22 @@ const Index = () => {
     return (
         <div>
             <AppHeader/>
-            <div className={Styles.wrapperButton}>
-                <div>
-                    <p>موتورت رو روشن کن</p>
+            <div className={Styles.wrapperHeader}>
+                <div className={Styles.itemHeader}>
+                    <p className={Styles.titleP}>موتورت رو روشن کن</p>
                 </div>
-                <button className={Styles.buttonLeft}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    <span>بررسی اطراف</span>
-                </button>
+                <div className={Styles.itemHeader}>
+                    <button className={Styles.buttonLeft}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        <span>بررسی اطراف</span>
+                    </button>
+                </div>
             </div>
             <div>
-                <p></p>
+                <p className={Styles.capthionP}>با کدوم همسفر ورزش میکنی جووون دل؟!</p>
+            </div>
+            <div className={Styles.wrapper}>
+                <h2 className={Styles.title}>ورزشیا</h2>
             </div>
             <Appbar/>
         </div>
