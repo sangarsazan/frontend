1e28365fba40b22b7cfcec0e90b9de4780b545b9
trumanshoow <dehghanalireza1214@gmail.com>
2023-11-02 18:46:10 +0330

    17...

---
+++ src/components/slider/dataSlider.js
@@ -0,0 +1,4 @@
+export default [
+    "/sliderImages/banner1.png",
+    "/sliderImages/banner2.png"
+]
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,35 @@
-import Styles from './home.module.css';
+import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPowerOff, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import Slider from '../../components/slider/slider'
+// import Slider from '../../components/slider/slider'
+import Slider from "react-slick";
+import dataSlider from "../../components/slider/dataSlider";
+import Styles from './home.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HomePage = () => {
-
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
     return (
         <div className={Styles.wrapper}>
-            <div>
-
+            <div className={Styles.sliderWrapper}>
+                <Slider className={Styles.slider} {...settings}>
+                    {dataSlider.map((obj) => {
+                        return (
+                            <div className={Styles.sliderItem}>
+                                <img
+                                    src={obj}
+                                />
+                            </div>
+                        )
+                    })}
+                </Slider>
             </div>
             <div className={Styles.wrapperButton}>
                 <button className={Styles.buttonRight}>
