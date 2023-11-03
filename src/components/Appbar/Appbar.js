328a35f0febb1687471ab3116acd0ced82b60741
trumanshoow <dehghanalireza1214@gmail.com>
2023-11-03 07:57:30 +0330

    aaaaaaaaaa

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -12,11 +12,8 @@ const Appbar = () => {
                     <MenuLink iconName={faComments} to="/chat">گفتگو</MenuLink>
                     <MenuLink iconName={faComments} to="/Services">خدمات</MenuLink>
                     <MenuLink iconName={faHouse} to="/home">خانه</MenuLink>
-<<<<<<< Updated upstream
                     <MenuLink iconName={faComments} to="/challenge">چالش</MenuLink>
-=======
                     <MenuLink iconName={faComments} to="/Challenge">چالش</MenuLink>
->>>>>>> Stashed changes
                     <MenuLink iconName={faUserGear} to="/profile">پروفایل</MenuLink>
                 </nav>
             </div>
--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -11,26 +11,21 @@ const Services = () => {
                 <botton>بررسی اطراف</botton>
                 <botton>رویداد ها</botton>
             </div>
+
             <div className={Styles.wrapper2}>
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
                 <botton>مشاوره</botton>
                 <botton>دوره های آموزشی</botton>
             </div>
-=======
->>>>>>> Stashed changes
+            <div>
                 <botton>مشاوره سلامت</botton>
                 <botton>دوره های آموزشی</botton>
             </div>
+            
             <div className={Styles.wrapper3}>
                 <botton>محاسبه BMI</botton>
                 <botton>فروشگاه تجهیزات</botton>
             </div>
-<<<<<<< Updated upstream
-=======
->>>>>>> 6236805d722b0621e7b99c49159bd8eaa8436088
->>>>>>> Stashed changes
+
             <Appbar/>
         </div>
     );
